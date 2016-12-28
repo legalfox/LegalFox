516795bba26f789f25de892702a38f6456def550
Evandro <roncolato@hotmail.com>
2016-12-27 23:59:10 -0200

    Menu e paginas

--- LegalFox/App_Code/cMenus.vb
+++ LegalFox/App_Code/cMenus.vb
@@ -10,14 +10,16 @@ Public Class cMenus
     Public Shared Function MontaMenu(_pai As Integer, Optional nivel As String = "") As String
         Try
 
-   
-        Dim obj As New cControle
-        Dim dt_menu As Data.DataTable
-        Dim ln_menu As Data.DataRow
-        Dim Conteudo As String = ""
+
+            Dim obj As New cControle
+            Dim dt_menu As Data.DataTable
+            Dim ln_menu As Data.DataRow
+            Dim dt_pagina As Data.DataTable
+            Dim ln_pagina As Data.DataRow
+            Dim Conteudo As String = ""
 
             'Seleciona todos os menus
-            dt_menu = obj.ExecutarBusca(sql_menu())
+            dt_menu = obj.ExecutarBusca(sql_menu(HttpContext.Current.Session("ESCRITORIO_ID")))
 
             Conteudo += "<div class='hor-menu'> "
             Conteudo += "<ul class='nav navbar-nav'> "
@@ -25,15 +27,25 @@ Public Class cMenus
             For Each ln_menu In dt_menu.Rows
                 Conteudo += "<li aria-haspopup='true' class='menu-dropdown classic-menu-dropdown active'> "
                 Conteudo += "   <a href='javascript:;'> " & ln_menu.Item("menu_label") & " <span class='arrow'></span> </a> "
-                Conteudo += "   <ul class='dropdown-menu pull-left'> "
-                Conteudo += "       <li aria-haspopup='true' class=' active'> "
-                Conteudo += "           <a href='index.html' class='nav-link  active'> "
-                Conteudo += "               <i class='icon-bar-chart'></i> Item 1 "
-                Conteudo += "           </a> "
-                Conteudo += "       </li> "
-                Conteudo += "</ul>"
-                'Conteudo += " < li Class='list-group-item'><a href='/categoria/" & ln_menu.Item("URL").ToString & "'><svg class='icon " & ln_menu.Item("ICONE").ToString & " glyph fs1'><use xlink:href='#" & ln_menu.Item("ICONE").ToString & "'></use></svg>" & ln_menu.Item("DESCRICAO").ToString & "</a></li>"
+
+                'Seleciona as paginas do menu
+                dt_pagina = obj.ExecutarBusca(sql_pagina(HttpContext.Current.Session("ESCRITORIO_ID"), ln_menu.Item("menu_id")))
+
+                For Each ln_pagina In dt_pagina.Rows
+                    Conteudo += "   <ul class='dropdown-menu pull-left'> "
+                    Conteudo += "       <li aria-haspopup='true' class=' '> "
+                    Conteudo += "           <a href='" & ln_pagina.Item("pagina_id") & "' class='nav-link  '> "
+                    Conteudo += "               <i class='" & ln_pagina.Item("icone") & "'></i> " & ln_pagina.Item("pagina_label") & " "
+                    Conteudo += "           </a> "
+                    Conteudo += "       </li> "
+                    Conteudo += "   </ul>"
+                Next
+
+                Conteudo += "</li>"
+
             Next
+            Conteudo += "</ul>"
+            Conteudo += "</div>"
 
 
             Return Conteudo
@@ -46,7 +58,12 @@ Public Class cMenus
 
 
     Public Shared Function sql_menu(_escritorio_id As String) As String
-        Return "SELECT escritorio_id, menu_id, menu_label FROM [legalfox_prd].[dbo].[lf_menus] WHERE escritorio_id = " & _escritorio_id & " AND STATUS = 'A' ORDER BY SEQ"
+        Return "SELECT menu_id, menu_label, icone FROM [legalfox_prd].[dbo].[lf_menus] WHERE escritorio_id = '" & _escritorio_id & "' AND status = 'A' ORDER BY sequencia"
     End Function
+
+    Public Shared Function sql_pagina(_escritorio_id As String, _menu_id As String) As String
+        Return "SELECT pagina_id, pagina_label, icone FROM [legalfox_prd].[dbo].[lf_paginas] where escritorio_id = '" & _escritorio_id & "' and menu_id = '" & _menu_id & "' ORDER BY sequencia"
+    End Function
+
 
 End Class
